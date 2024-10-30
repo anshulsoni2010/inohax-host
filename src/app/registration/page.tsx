d66428717529b99e8f3222b423d21522edc6a8f3
Anshul Soni <anshulsoni2022@gmail.com>
2024-10-30 15:49:32 +0530

    update

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { ChevronRight, User, Phone, Mail } from 'lucide-react'
+import { ChevronRight } from 'lucide-react'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
