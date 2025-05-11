a2feb6aff5fe6c77a72c54162ffcd1c0b9c7621c
Anshul Soni <anshulsoni2022@gmail.com>
2025-05-11 12:11:53 +0530

    updated clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -29,18 +30,17 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
-                <script
-                    type="text/javascript"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "oolt1oy5wj");
-            `,
-                    }}
-                />
+              <Script id="microsoft-clarity" strategy="afterInteractive">
+                {`
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "rhrj0hrue1");
+                `}
+              </Script>
+
+              
                 <link rel="icon" href="/inovact.png" />
                 <meta name="description" content="Inohax 2.0 - A 24 Hours Online Open Innovation Hackathon by Inovact for Students & Entrepreneurs, May 23-24, 2025" />
                 <meta property="og:url" content="https://inohax.inovact.in" />
